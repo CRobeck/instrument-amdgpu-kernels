7ef1f789ea70ef7ce5f5c560eb220bb25076f68a
Corbin Robeck <corbin.robeck@amd.com>
2024-04-02 20:14:58 -0500

    load/store address space

--- lib/DumpAMDGCNRegisters.cpp
+++ lib/DumpAMDGCNRegisters.cpp
@@ -13,7 +13,7 @@ using namespace llvm;
 bool DumpAMDGCNRegisters::runOnModule(Module &M) {
   bool ModifiedCodeGen = false;
   auto &CTX = M.getContext();
-  InlineAsm* V;
+  // InlineAsm* V;
   for (auto &F : M) {
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
       FunctionType *FT = FunctionType::get(Type::getVoidTy(CTX),
@@ -24,34 +24,32 @@ bool DumpAMDGCNRegisters::runOnModule(Module &M) {
           cast<Function>(InjectedFunctionCallee.getCallee());
       errs() << "Function To Be Injected: " << InjectedFunction->getName()
              << "\n";
-        // for(Function::iterator BB = F.begin();BB!=F.end();BB++){
-        //   for(BasicBlock::iterator I = BB->begin();I!=BB->end();I++){  
-        //     if (isa<CallInst>(I) || isa<InvokeInst>(I)) {
-        //       const CallBase &cb = cast<CallBase>(*I);
-        //       if (const InlineAsm *IA = dyn_cast<InlineAsm>(cb.getCalledOperand())) {
-        //         const std::string &AsmString = IA->getAsmString();
-        //         const std::string &ConstraintStr = IA->getConstraintString();
-        //         errs() <<  *IA << "\n";  
-        //         errs() << "AsmString: " << AsmString << "\n";  
-        //         errs() << "ConstraintStr: " << ConstraintStr << "\n"; 
-        //         errs() << *IA->getFunctionType() << "\n";   
-                // for (llvm::Function::arg_iterator arg = kernelFunc->arg_begin();
-                //       arg != kernelFunc->arg_end(); ++arg)                
-                // V = InlineAsm::get(IA->getFunctionType(), IA->getAsmString(), IA->getConstraintString(),
-                //                    IA->hasSideEffects(), IA->isAlignStack(),
-                //                    IA->getDialect(), IA->canThrow()); 
+        for(Function::iterator BB = F.begin();BB!=F.end();BB++){
+          for(BasicBlock::iterator I = BB->begin();I!=BB->end();I++){  
+			if (auto LI = dyn_cast<LoadInst>(I)){
+ 				Value *PO = LI->getPointerOperand();
+				PointerType *PTy = cast<PointerType>(PO->getType());
+				unsigned AddrSpace = PTy->getAddressSpace();
+              	errs() << *LI << "\n";			
+				errs() << "Address Space: " << AddrSpace << "\n";
             }
-          }
+		    if (auto SI = dyn_cast<StoreInst>(I)){
+ 				Value *PO = SI->getPointerOperand();
+				PointerType *PTy = cast<PointerType>(PO->getType());
+				unsigned AddrSpace = PTy->getAddressSpace();
+              	errs() << *SI << "\n";			
+				errs() << "Address Space: " << AddrSpace << "\n";
+			}
         }
     }             
 
+//            if (isa<LoadInst>(I) || isa<StoreInst>(I)) {
       FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), Type::getInt32Ty(CTX), false);
       // std::string AsmString = "v_mov_b32 $0 v0\n";
       std::string AsmString = "s_mov_b32 m0 $1\ns_mov_b32 $0 m0\n";
 
       InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,I", false); 
            
-      errs() << *FTy << "\n";   
       // // Get an IR builder. Sets the insertion point to the top of the function
       IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
       Value* UniqueInt = Builder.getInt32(5);
