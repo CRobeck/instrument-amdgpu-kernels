b0e29ce7fd43fff3d62c05fab4c99744796dee56
Corbin Robeck <corbin.robeck@amd.com>
2024-04-03 21:41:18 -0500

    working lds storettrace injection

--- DumpAMDGCNRegisters/vectorAdd.cpp
+++ DumpAMDGCNRegisters/vectorAdd.cpp
@@ -5,90 +5,115 @@
 #include <iostream>
 #include <string>
 
-__global__ void vecAdd(int n, double *a, double *b, double *c) {
-int out;
-int tid = threadIdx.x;
-__asm__ __volatile__(\
-"s_mov_b32 m0 %1\n"\
-"s_mov_b32 %0 m0\n" \
-: "=s"(out) : "I" (5));
-printf("Thread %d m0 value: %d\n", tid, out);
-  int id = blockIdx.x * blockDim.x + threadIdx.x; 
-  if (id < n)
-    c[id] = a[id] + b[id];
+#define SHARED_SIZE 2
+
+//__device__ uint32_t ttrace_counter = 0;
+__global__ void kernel(int offset)
+{
+ int out;
+// __asm__ __volatile__("s_add_i32 %1 %1 1\n"\
+//                       : "=s"(out) : "s" (ttrace_counter));
+    __shared__ uint32_t sharedMem[SHARED_SIZE];
+
+    if (threadIdx.x == 0)
+        for (int i = 0; i < SHARED_SIZE; i++) sharedMem[i] = 0;
+    __syncthreads();
+
+    // repeatedly read and write to shared memory
+    uint32_t index = threadIdx.x * offset;
+    for (int i = 0; i < 10000; i++)
+    {
+        sharedMem[index] += index * i;
+        index += 32;
+        index %= SHARED_SIZE;
+    }
 }
+
+
+// __global__ void vecAdd(int n, double *a, double *b, double *c) {
+// int out;
+// int tid = threadIdx.x;
+
+// __asm__ __volatile__("s_mov_b32 m0 %1\n"\
+//                      "s_mov_b32 %0 m0\n"\
+//                       : "=s"(out) : "I" (5));
+// printf("Thread %d m0 value: %d\n", tid, out);
+//   int id = blockIdx.x * blockDim.x + threadIdx.x; 
+//   if (id < n)
+//     c[id] = a[id] + b[id];
+// }
 
 int main(int argc, char *argv[]) {
 
   // Size of vectors
-  int n = 1024;
+  int n = 32;
 
   // Host input vectors
-  double *h_a;
-  double *h_b;
-  // Host output vector
-  double *h_c;
+  // double *h_a;
+  // double *h_b;
+  // // Host output vector
+  // double *h_c;
 
-  // Device input vectors
-  double *d_a;
-  double *d_b;
-  // Device output vector
-  double *d_c;
+  // // Device input vectors
+  // double *d_a;
+  // double *d_b;
+  // // Device output vector
+  // double *d_c;
 
-  // Size, in bytes, of each vector
-  size_t bytes = n * sizeof(double);
+  // // Size, in bytes, of each vector
+  // size_t bytes = n * sizeof(double);
 
-  // Allocate memory for each vector on host
-  h_a = (double *)malloc(bytes);
-  h_b = (double *)malloc(bytes);
-  h_c = (double *)malloc(bytes);
+  // // Allocate memory for each vector on host
+  // h_a = (double *)malloc(bytes);
+  // h_b = (double *)malloc(bytes);
+  // h_c = (double *)malloc(bytes);
 
-  // Allocate memory for each vector on GPU
-  (void)hipMalloc(&d_a, bytes);
-  (void)hipMalloc(&d_b, bytes);
-  (void)hipMalloc(&d_c, bytes);
+  // // Allocate memory for each vector on GPU
+  // (void)hipMalloc(&d_a, bytes);
+  // (void)hipMalloc(&d_b, bytes);
+  // (void)hipMalloc(&d_c, bytes);
 
-  int i;
-  // Initialize vectors on host
-  for (i = 0; i < n; i++) {
-    h_a[i] = sin(i) * sin(i);
-    h_b[i] = cos(i) * cos(i);
-  }
+  // int i;
+  // // Initialize vectors on host
+  // for (i = 0; i < n; i++) {
+  //   h_a[i] = sin(i) * sin(i);
+  //   h_b[i] = cos(i) * cos(i);
+  // }
 
-  // Copy host vectors to device
-  (void)hipMemcpy(d_a, h_a, bytes, hipMemcpyHostToDevice);
-  (void)hipMemcpy(d_b, h_b, bytes, hipMemcpyHostToDevice);
+  // // Copy host vectors to device
+  // (void)hipMemcpy(d_a, h_a, bytes, hipMemcpyHostToDevice);
+  // (void)hipMemcpy(d_b, h_b, bytes, hipMemcpyHostToDevice);
 
-  int blockSize, gridSize;
+  // int blockSize, gridSize;
 
-  // Number of threads in each thread block
-  blockSize = 1024;
+  // // Number of threads in each thread block
+  // blockSize = 32;
 
-  // Number of thread blocks in grid
-  gridSize = (int)ceil((float)n / blockSize);
+  // // Number of thread blocks in grid
+  // gridSize = (int)ceil((float)n / blockSize);
 
   // Execute the kernel
-  vecAdd<<<1, 5>>>(n, d_a, d_b, d_c);
+  kernel<<<1, 5>>>(n);
 
   // Copy array back to host
-  (void)hipMemcpy(h_c, d_c, bytes, hipMemcpyDeviceToHost);
+  // (void)hipMemcpy(h_c, d_c, bytes, hipMemcpyDeviceToHost);
 
-  // Sum up vector c and print result divided by n, this should equal 1 within
-  // error
-  double sum = 0;
-  for (i = 0; i < n; i++)
-    sum += h_c[i];
-  printf("Result (should be 1.0): %f\n", sum / n);
+  // // Sum up vector c and print result divided by n, this should equal 1 within
+  // // error
+  // double sum = 0;
+  // for (i = 0; i < n; i++)
+  //   sum += h_c[i];
+  // printf("Result (should be 1.0): %f\n", sum / n);
 
-  // Release device memory
-  (void)hipFree(d_a);
-  (void)hipFree(d_b);
-  (void)hipFree(d_c);
+  // // Release device memory
+  // (void)hipFree(d_a);
+  // (void)hipFree(d_b);
+  // (void)hipFree(d_c);
 
-  // Release host memory
-  free(h_a);
-  free(h_b);
-  free(h_c);
+  // // Release host memory
+  // free(h_a);
+  // free(h_b);
+  // free(h_c);
 
   return 0;
 }
--- lib/DumpAMDGCNRegisters.cpp
+++ lib/DumpAMDGCNRegisters.cpp
@@ -13,56 +13,87 @@ using namespace llvm;
 bool DumpAMDGCNRegisters::runOnModule(Module &M) {
   bool ModifiedCodeGen = false;
   auto &CTX = M.getContext();
+  IRBuilder<> ModuleBuilder(CTX);
+  Value* TtraceCounter = ModuleBuilder.getInt32(0);
+  unsigned UniqueInt = 0;
+//  if(!InjectedTtraceCounter)
+//	errs() << "Global var not found" << "\n";   
+//  GlobalVariable *TtraceCounter = 
+//      new GlobalVariable(/*Module=*/M,
+//                         /*Type=*/Type::getInt32Ty(CTX),
+//                         /*isConstant=*/false,
+//                         /*Linkage=*/GlobalValue::InternalLinkage,
+//                         /*Initializer=*/0, 
+//                         /*Name=*/"ttrace_counter");
   // InlineAsm* V;
   for (auto &F : M) {
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
-      FunctionType *FT = FunctionType::get(Type::getVoidTy(CTX),
-                                           {Type::getInt32Ty(CTX)}, false);
-      FunctionCallee InjectedFunctionCallee =
-          M.getOrInsertFunction("_Z11PrintKerneli", FT);
-      Function *InjectedFunction =
-          cast<Function>(InjectedFunctionCallee.getCallee());
-      errs() << "Function To Be Injected: " << InjectedFunction->getName()
-             << "\n";
         for(Function::iterator BB = F.begin();BB!=F.end();BB++){
           for(BasicBlock::iterator I = BB->begin();I!=BB->end();I++){  
-			if (auto LI = dyn_cast<LoadInst>(I)){
- 				Value *PO = LI->getPointerOperand();
-				PointerType *PTy = cast<PointerType>(PO->getType());
-				unsigned AddrSpace = PTy->getAddressSpace();
-              	errs() << *LI << "\n";			
-				errs() << "Address Space: " << AddrSpace << "\n";
-            }
-		    if (auto SI = dyn_cast<StoreInst>(I)){
- 				Value *PO = SI->getPointerOperand();
-				PointerType *PTy = cast<PointerType>(PO->getType());
-				unsigned AddrSpace = PTy->getAddressSpace();
+			// if (auto LI = dyn_cast<LoadInst>(I)){
+      //   GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(LI->getPointerOperand());
+      //   Value *Op = GEPInst->getPointerOperand()->stripPointerCasts();
+			// 	unsigned AddrSpace = cast<PointerType>(Op->getType())->getAddressSpace();
+      //         	errs() << *LI << "\n";			
+			// 	errs() << "Address Space: " << AddrSpace << "\n";
+      //   if (AddrSpace == 3){
+      //       FunctionType *FTy = FunctionType::get(Type::getVoidTy(CTX), false);
+      //       std::string AsmString = "s_mov_b32 m0 " + std::to_string(UniqueInt) + "\n";
+      //       IRBuilder<> Builder(LI);
+      //       InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "", false);
+      //       Builder.CreateCall(InlineAsmFunc, {});
+      //       I++;
+      //       Builder.SetInsertPoint(dyn_cast<Instruction>(I));
+      //       I--;
+      //       Builder.CreateCall(InlineAsm::get(FTy, "s_mov_b32 m0 53\n", "", false), {});            
+      //       UniqueInt++;
+      //   }
+      //       }
+
+			if (auto SI = dyn_cast<StoreInst>(I)){
+        GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(SI->getPointerOperand());
+        Value *Op = GEPInst->getPointerOperand()->stripPointerCasts();
+				unsigned AddrSpace = cast<PointerType>(Op->getType())->getAddressSpace();
               	errs() << *SI << "\n";			
 				errs() << "Address Space: " << AddrSpace << "\n";
-			}
+        if (AddrSpace == 3){
+            FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), true);
+//			GlobalVariable *TtraceCounter = M.getNamedGlobal("ttrace_counter");
+            // std::string AsmString = "s_mov_b32 m0 " + std::to_string(UniqueInt) + "\n""s_nop 0\n""s_mov_b32 m0 53\n";
+            std::string AsmString = "s_mov_b32 $0 m0\ns_nop 0\n""s_mov_b32 m0 $1""\n""s_nop 0\n""s_ttracedata\n""s_nop 0\n""s_mov_b32 m0 $0\n""s_nop 0\n""s_add_i32 $1 $1 1\n";
+            IRBuilder<> Builder(SI);
+            InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,s", true);
+            Builder.CreateCall(InlineAsmFunc, {TtraceCounter});
+            UniqueInt++;
+            return true;
         }
-    }             
+            }
 
-//            if (isa<LoadInst>(I) || isa<StoreInst>(I)) {
-      FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), Type::getInt32Ty(CTX), false);
-      // std::string AsmString = "v_mov_b32 $0 v0\n";
-      std::string AsmString = "s_mov_b32 m0 $1\ns_mov_b32 $0 m0\n";
 
-      InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,I", false); 
+        }
+    } //End of instructions in AMDGCN kernel loop
+
+    errs() << "Injected " << UniqueInt << " LDS Load/Store s_ttrace instructions\n"; 
+       
+
+      // FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), Type::getInt32Ty(CTX), false);
+      // std::string AsmString = "s_mov_b32 m0 $1\ns_mov_b32 $0 m0\n";
+
+      // InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,I", true); 
            
-      // // Get an IR builder. Sets the insertion point to the top of the function
-      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
-      Value* UniqueInt = Builder.getInt32(5);
+      // // // Get an IR builder. Sets the insertion point to the top of the function
+      // IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+      // Value* UniqueInt = Builder.getInt32(5);
 
-      Value *InlineAsmFuncResult = Builder.CreateCall(InlineAsmFunc, {UniqueInt});
-      Builder.CreateCall(InjectedFunction, {InlineAsmFuncResult});
+      // Builder.CreateCall(InlineAsmFunc, {UniqueInt});
+      // // Builder.CreateCall(InjectedFunction, {InlineAsmFuncResult});
 
-      errs() << "Injecting Device Function Into AMDGPU Kernel: " << F.getName()
-             << "\n";
+      // errs() << "Injecting Device Function Into AMDGPU Kernel: " << F.getName()
+      //        << "\n";
 
       ModifiedCodeGen = true;
-    }
-  }
+    } //End of if AMDGCN Kernel
+  } //End of functions in module loop
   return ModifiedCodeGen;
 }
 
