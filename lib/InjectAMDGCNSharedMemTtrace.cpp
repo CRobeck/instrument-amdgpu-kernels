db5f7935d369d6220ea8ffdbfea6a15650ae94e9
Corbin Robeck <corbin.robeck@amd.com>
2024-04-05 21:03:23 -0500

    formatting

--- lib/InjectAMDGCNSharedMemTtrace.cpp
+++ lib/InjectAMDGCNSharedMemTtrace.cpp
@@ -1,7 +1,7 @@
 #include "InjectAMDGCNSharedMemTtrace.h"
 
-#include "llvm/IR/InlineAsm.h"
 #include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/IR/InlineAsm.h"
 #include "llvm/IR/IntrinsicsAMDGPU.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
@@ -15,104 +15,157 @@ bool InjectAMDGCNSharedMemTtrace::runOnModule(Module &M) {
   auto &CTX = M.getContext();
   bool DebugInfoWarningPrinted = false;
   IRBuilder<> ModuleBuilder(CTX);
-  //This is the actual variable value that gets inserted in the Inline ASM
-  Value* TtraceCounter = ModuleBuilder.getInt32(0);
-  //This is the internal counter in the compiler pass. These two will not match currently b/c
-  //unrolled loops will copy/inline the InlineASM version not the internal compiler counter.
+  // This is the actual variable value that gets inserted in the Inline ASM
+  Value *TtraceCounter = ModuleBuilder.getInt32(0);
+  // This is the internal counter in the compiler pass. These two will not match
+  // currently b/c unrolled loops will copy/inline the InlineASM version not the
+  // internal compiler counter.
   unsigned CounterInt = 0;
   for (auto &F : M) {
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
-        for(Function::iterator BB = F.begin();BB!=F.end();BB++){
-          for(BasicBlock::iterator I = BB->begin();I!=BB->end();I++){
-		  	//Shared memory reads
-			if (auto LI = dyn_cast<LoadInst>(I)){
-//        GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(LI->getPointerOperand());
-        Value *Op = LI->getPointerOperand()->stripPointerCasts();
-				unsigned AddrSpace = cast<PointerType>(Op->getType())->getAddressSpace();
-        if (AddrSpace == 3){
-if (DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc()) { 
-    				  std::string SourceInfo = (F.getName() + " " + DL->getFilename() + ":" + Twine(DL->getLine()) + ":" +
-    				         Twine(DL->getColumn())).str();
-					  errs() << CounterInt << " " << SourceInfo  << "\n";
-					}
-			else{
-				if(!DebugInfoWarningPrinted){
-					errs() << "warning: no debug info found, did you forget to add -ggdb?\n";
-					DebugInfoWarningPrinted = true;
-				}
-			}
-            FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), true);
-            std::string AsmString = "s_mov_b32 $0 m0\n""s_mov_b32 m0 $1""\n""s_nop 0\n""s_ttracedata\n""s_mov_b32 m0 $0\n""s_add_i32 $1 $1 1\n";
-            IRBuilder<> Builder(LI);
-            InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,s", true);
-            Builder.CreateCall(InlineAsmFunc, {TtraceCounter});
-            I++;
-            Builder.SetInsertPoint(dyn_cast<Instruction>(I));
-            Builder.CreateCall(InlineAsm::get(FTy, "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n"\
-                                                   "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n"\
-                                                   "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n", "", false), {});      
-			I--;
+      for (Function::iterator BB = F.begin(); BB != F.end(); BB++) {
+        for (BasicBlock::iterator I = BB->begin(); I != BB->end(); I++) {
+          // Shared memory reads
+          if (auto LI = dyn_cast<LoadInst>(I)) {
+            //        GetElementPtrInst *GEPInst =
+            //        dyn_cast<GetElementPtrInst>(LI->getPointerOperand());
+            Value *Op = LI->getPointerOperand()->stripPointerCasts();
+            unsigned AddrSpace =
+                cast<PointerType>(Op->getType())->getAddressSpace();
+            if (AddrSpace == 3) {
+              if (DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc()) {
+                std::string SourceInfo =
+                    (F.getName() + " " + DL->getFilename() + ":" +
+                     Twine(DL->getLine()) + ":" + Twine(DL->getColumn()))
+                        .str();
+                errs() << CounterInt << " " << SourceInfo << "\n";
+              } else {
+                if (!DebugInfoWarningPrinted) {
+                  errs() << "warning: no debug info found, did you forget to "
+                            "add -ggdb?\n";
+                  DebugInfoWarningPrinted = true;
+                }
+              }
+              FunctionType *FTy =
+                  FunctionType::get(Type::getInt32Ty(CTX), true);
+              std::string AsmString = "s_mov_b32 $0 m0\n"
+                                      "s_mov_b32 m0 $1"
+                                      "\n"
+                                      "s_nop 0\n"
+                                      "s_ttracedata\n"
+                                      "s_mov_b32 m0 $0\n"
+                                      "s_add_i32 $1 $1 1\n";
+              IRBuilder<> Builder(LI);
+              InlineAsm *InlineAsmFunc =
+                  InlineAsm::get(FTy, AsmString, "=s,s", true);
+              Builder.CreateCall(InlineAsmFunc, {TtraceCounter});
+              I++;
+              Builder.SetInsertPoint(dyn_cast<Instruction>(I));
+              Builder.CreateCall(InlineAsm::get(FTy,
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n",
+                                                "", false),
+                                 {});
+              I--;
 
-            CounterInt++;
-        }
+              CounterInt++;
             }
-			//Shared memory writes
-			if (auto SI = dyn_cast<StoreInst>(I)){
-//        GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(SI->getPointerOperand());
-        Value *Op = SI->getPointerOperand()->stripPointerCasts();
-				unsigned AddrSpace = cast<PointerType>(Op->getType())->getAddressSpace();
-        if (AddrSpace == 3){
-if (DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc()) { 
-    				  std::string SourceInfo = (F.getName() + " " + DL->getFilename() + ":" + Twine(DL->getLine()) + ":" +
-    				         Twine(DL->getColumn())).str();
-					  errs() << CounterInt << " " << SourceInfo  << "\n";
-					}
-			else{
-				if(!DebugInfoWarningPrinted){
-					errs() << "warning: no debug info found, did you forget to add -ggdb?\n";
-					DebugInfoWarningPrinted = true;
-				}
-			}
-            FunctionType *FTy = FunctionType::get(Type::getInt32Ty(CTX), true);
-            std::string AsmString = "s_mov_b32 $0 m0\n""s_mov_b32 m0 $1""\n""s_nop 0\n""s_ttracedata\n""s_mov_b32 m0 $0\n""s_add_i32 $1 $1 1\n";
-            IRBuilder<> Builder(SI);
-            InlineAsm* InlineAsmFunc = InlineAsm::get(FTy, AsmString, "=s,s", true);
-            Builder.CreateCall(InlineAsmFunc, {TtraceCounter});
-            I++;
-            Builder.SetInsertPoint(dyn_cast<Instruction>(I));
-            Builder.CreateCall(InlineAsm::get(FTy, "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n"\
-                                                   "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n"\
-                                                   "s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n""s_nop 15\n", "", true), {});      
-			I--;
-            CounterInt++;
-        }
+          }
+          // Shared memory writes
+          if (auto SI = dyn_cast<StoreInst>(I)) {
+            //        GetElementPtrInst *GEPInst =
+            //        dyn_cast<GetElementPtrInst>(SI->getPointerOperand());
+            Value *Op = SI->getPointerOperand()->stripPointerCasts();
+            unsigned AddrSpace =
+                cast<PointerType>(Op->getType())->getAddressSpace();
+            if (AddrSpace == 3) {
+              if (DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc()) {
+                std::string SourceInfo =
+                    (F.getName() + " " + DL->getFilename() + ":" +
+                     Twine(DL->getLine()) + ":" + Twine(DL->getColumn()))
+                        .str();
+                errs() << CounterInt << " " << SourceInfo << "\n";
+              } else {
+                if (!DebugInfoWarningPrinted) {
+                  errs() << "warning: no debug info found, did you forget to "
+                            "add -ggdb?\n";
+                  DebugInfoWarningPrinted = true;
+                }
+              }
+              FunctionType *FTy =
+                  FunctionType::get(Type::getInt32Ty(CTX), true);
+              std::string AsmString = "s_mov_b32 $0 m0\n"
+                                      "s_mov_b32 m0 $1"
+                                      "\n"
+                                      "s_nop 0\n"
+                                      "s_ttracedata\n"
+                                      "s_mov_b32 m0 $0\n"
+                                      "s_add_i32 $1 $1 1\n";
+              IRBuilder<> Builder(SI);
+              InlineAsm *InlineAsmFunc =
+                  InlineAsm::get(FTy, AsmString, "=s,s", true);
+              Builder.CreateCall(InlineAsmFunc, {TtraceCounter});
+              I++;
+              Builder.SetInsertPoint(dyn_cast<Instruction>(I));
+              Builder.CreateCall(InlineAsm::get(FTy,
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n"
+                                                "s_nop 15\n",
+                                                "", true),
+                                 {});
+              I--;
+              CounterInt++;
             }
-
-
+          }
         }
-    } //End of instructions in AMDGCN kernel loop
+      } // End of instructions in AMDGCN kernel loop
 
-    errs() << "Injected LDS Load/Store s_ttrace instructions at " << CounterInt <<
-               " source locations\n"; 
-       
+      errs() << "Injected LDS Load/Store s_ttrace instructions at "
+             << CounterInt << " source locations\n";
 
       ModifiedCodeGen = true;
-    } //End of if AMDGCN Kernel
-  } //End of functions in module loop
+    } // End of if AMDGCN Kernel
+  }   // End of functions in module loop
   return ModifiedCodeGen;
 }
 
 PassPluginLibraryInfo getPassPluginInfo() {
   const auto callback = [](PassBuilder &PB) {
-    PB.registerOptimizerLastEPCallback(
-        [&](ModulePassManager &MPM, auto) {
-          MPM.addPass(InjectAMDGCNSharedMemTtrace());
-          return true;
-        });
+    PB.registerOptimizerLastEPCallback([&](ModulePassManager &MPM, auto) {
+      MPM.addPass(InjectAMDGCNSharedMemTtrace());
+      return true;
+    });
   };
 
-  return {LLVM_PLUGIN_API_VERSION, "inject-amdgcn-lds-ttrace", LLVM_VERSION_STRING,
-          callback};
+  return {LLVM_PLUGIN_API_VERSION, "inject-amdgcn-lds-ttrace",
+          LLVM_VERSION_STRING, callback};
 };
 
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
