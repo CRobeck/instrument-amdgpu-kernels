f94bc652d4107e84738bc8afbfaefe320b9b6824
Corbin Robeck <corbin.robeck@amd.com>
2024-04-29 09:21:53 -0500

    add generalize instrumentation functions and load store identifier to ttrace value

--- lib/AMDGCNMemCoalescing.cpp
+++ lib/AMDGCNMemCoalescing.cpp
@@ -17,14 +17,18 @@ using namespace std;
 
 static cl::list<std::string>
     AMDGCNKernelsToInstrument("amdgcn-kernels-to-instrument",
-                       cl::desc("Specify function(s) to extract using a "
-                                "regular expression"),
-                       cl::value_desc("rkernels"));
+                       cl::desc("Specify function(s) to instrument using a "
+                                "regular expression"));
+
+static cl::opt<std::string> AMDGCNInstrumentationFunction("amdgcn-instrumentation-function",
+                       cl::desc("Specify function to inject"));      
+
+static cl::opt<std::string> AMDGCNInstrumentationPoint("amdgcn-instrumentation-point",
+                       cl::desc("Specify point in function inject instrumentation-function"));                         
 
 bool AMDGCNMemCoalescing::runOnModule(Module &M) {
   bool ModifiedCodeGen = false;
   auto &CTX = M.getContext();
-  StringRef moduleName = M.getName();
   uint32_t TtraceCounterInt = 1;    
   for (auto &F : M) {
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
@@ -43,45 +47,36 @@ bool AMDGCNMemCoalescing::runOnModule(Module &M) {
               if(AddrSpace == 3 || AddrSpace == 4) continue;
               
               StringRef UnmangledName = getUnmangledName(F.getName());
-              Value* moduleVal = Builder.CreateGlobalStringPtr(moduleName);
-              Value* functionVal = Builder.CreateGlobalStringPtr(UnmangledName);
-              Value* storeVal = Builder.CreateGlobalStringPtr("Load");
+              // Value* storeVal = Builder.CreateGlobalStringPtr("Load");
+              Value* loadVal = Builder.getInt32(1); //0=Store, 1=Load
 
               DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc();
-              uint32_t lineNum = DL->getLine();
-              uint32_t columnNum = DL->getColumn();  
-			        Value* fileVal = Builder.CreateGlobalStringPtr(DL->getFilename());
-              Value *line = Builder.getInt32(lineNum);
-              Value *column = Builder.getInt32(columnNum);   
 
               std::string SourceInfo =
-                  (F.getName() + "\t" + DL->getFilename() + ":" +
+                  (F.getName() + "     " + DL->getFilename() + ":" +
                    Twine(DL->getLine()) + ":" + Twine(DL->getColumn()))
                       .str();
-              errs() << TtraceCounterInt << "\t" << SourceInfo << "\n";
+              errs() << TtraceCounterInt << "     " << SourceInfo << "\n";
 
-              Type* baseType = LI->getType();
+              Type* dataType = LI->getType();
               DataLayout* dl = new DataLayout(&M);
-              uint32_t typeSize = dl->getTypeStoreSize(baseType); 
-              Value *typeSizeB = Builder.getInt32(typeSize);
+              uint32_t typeSize = dl->getTypeStoreSize(dataType); 
+              Value *typeSizeVal = Builder.getInt32(typeSize);
 
               FunctionType *FT = FunctionType::get(Type::getInt32Ty(CTX),
-                                                   {cast<PointerType>(GEP->getType()), cast<PointerType>(moduleVal->getType()),
-                                                   cast<PointerType>(functionVal->getType()), cast<PointerType>(storeVal->getType()),
-                                                   Type::getInt32Ty(CTX), Type::getInt32Ty(CTX), Type::getInt32Ty(CTX), 
-                                                   Type::getInt32Ty(CTX)}, false);                               
+                                                   {cast<PointerType>(GEP->getType()),
+                                                   Type::getInt32Ty(CTX),
+                                                   Type::getInt32Ty(CTX), Type::getInt32Ty(CTX)}, false);                               
               FunctionCallee InjectedFunctionCallee =
-                  M.getOrInsertFunction("_Z15countCacheLinesPvPcS0_S0_jjjj", FT);
+                  M.getOrInsertFunction("_Z15countCacheLinesPvjjj", FT);
               FunctionCallee PrintFunctionCallee =
-                  M.getOrInsertFunction("_Z11PrintKernelj", FunctionType::get(Type::getVoidTy(CTX), {Type::getInt32Ty(CTX)}, false));     
+                  M.getOrInsertFunction("_Z15PrintCacheLinesj", FunctionType::get(Type::getVoidTy(CTX), {Type::getInt32Ty(CTX)}, false));     
               Function *PrintFunction =
                   cast<Function>(PrintFunctionCallee.getCallee());                                 
-              Value* NumCacheLines = Builder.CreateCall(InjectedFunctionCallee, {Addr, fileVal, functionVal, storeVal, line, column, TtraceCounterIntVal, typeSizeB});
+              Value* NumCacheLines = Builder.CreateCall(InjectedFunctionCallee, {Addr, loadVal, TtraceCounterIntVal, typeSizeVal});
               errs() << "Injecting Mem Coalescing Function Into AMDGPU Kernel: " << UnmangledName
                      << "\n";      
-              Builder.CreateCall(PrintFunction, {NumCacheLines}); 
-              SendValueToTraceStream(dyn_cast<Instruction>(I), 
-              // Builder.CreateCall(PrintFunction, {NumCacheLines});                     
+              Builder.CreateCall(PrintFunction, {NumCacheLines});                   
               TtraceCounterInt++;   
               ModifiedCodeGen = true;                                                                     
           }
