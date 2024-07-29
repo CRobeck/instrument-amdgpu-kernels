59c9f97c24893f809fbd85cf42dad75dbaeea74c
Corbin Robeck <corbin.robeck@amd.com>
2024-07-29 14:53:54 +0000

    add address space and load/store reporting

--- lib/AMDGCNMemTrace.cpp
+++ lib/AMDGCNMemTrace.cpp
@@ -7,37 +7,47 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Bitcode/BitcodeWriter.h"
 #include <iostream>
 #include <vector>
 using namespace llvm;
 using namespace std;
 
-template <typename LoadOrStoreInst>
+std::map<int, std::string> AddrSpaceMap = {{0, "FLAT"},
+					   {1, "GLOBAL"},
+					   {3, "SHARED"},
+					   {4, "CONSTANT"}};
+
+std::string LoadOrStoreMap(const BasicBlock::iterator &I){
+		if (LoadInst* LI = dyn_cast<LoadInst>(I)) return "LOAD";
+		else if (StoreInst* SI = dyn_cast<StoreInst>(I)) return "STORE";
+		else throw std::runtime_error("Error: unknown operation type");
+}
+
+template <typename LoadOrStoreInst> 
 void InjectingInstrumentationFunction(const BasicBlock::iterator &I, const Function &F, const llvm::Module &M,
-                                      uint32_t &LocationCounter){
-        auto &CTX = M.getContext();
-        auto LSI = dyn_cast<LoadOrStoreInst>(I);
-        if (not LSI) return;
-        IRBuilder<> Builder(dyn_cast<Instruction>(I));
-        Value *Addr = LSI->getPointerOperand();
-        Value *LocationCounterVal = Builder.getInt32(LocationCounter);
-        Value *Op = LSI->getPointerOperand()->stripPointerCasts();
+				      uint32_t &LocationCounter){
+	auto &CTX = M.getContext();
+	auto LSI = dyn_cast<LoadOrStoreInst>(I);
+	if (not LSI) return;
+	IRBuilder<> Builder(dyn_cast<Instruction>(I));
+	Value *Addr = LSI->getPointerOperand();
+	Value *LocationCounterVal = Builder.getInt32(LocationCounter);
+	Value *Op = LSI->getPointerOperand()->stripPointerCasts();
         uint32_t AddrSpace =
                   cast<PointerType>(Op->getType())->getAddressSpace();
-        DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc();
+	DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc();
 
         std::string SourceInfo =
             (F.getName() + "     " + DL->getFilename() + ":" +
              Twine(DL->getLine()) + ":" + Twine(DL->getColumn()))
-                .str();
-
+                .str();	
+	
         Function *InstrumentationFunction = M.getFunction("_Z8memTracePvj");
         Builder.CreateCall(FunctionType::get(Type::getVoidTy(CTX), {Addr->getType(), Type::getInt32Ty(CTX)} ,false), InstrumentationFunction, {Addr, LocationCounterVal});
         errs() << "Injecting Mem Trace Function Into AMDGPU Kernel: " << SourceInfo
                << "\n";
-        errs() << LocationCounter << "     " << SourceInfo << "\n";
-        LocationCounter++;
+        errs() << LocationCounter << "     " << SourceInfo <<  "     " << AddrSpaceMap[AddrSpace] << "     " << LoadOrStoreMap(I) << "\n";
+	LocationCounter++;
 }
 
 
@@ -53,23 +63,23 @@ bool AMDGCNMemTrace::runOnModule(Module &M) {
                errorMsg.c_str());
     exit(1);
   }
-  Linker::linkModules(M, std::move(InstrumentationModule));
+  Linker::linkModules(M, std::move(InstrumentationModule));  
   for (auto &F : M) {
-    if(F.isIntrinsic() || F.getName() == "_Z8memTracePvj") continue;
+    if(F.isIntrinsic()) continue;
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
       for (Function::iterator BB = F.begin(); BB != F.end(); BB++) {
         for (BasicBlock::iterator I = BB->begin(); I != BB->end(); I++) {
-          if (LoadInst* LI = dyn_cast<LoadInst>(I)) {
-              InjectingInstrumentationFunction<LoadInst>(I, F, M, LocationCounter);
-              ModifiedCodeGen = true;
+          if (LoadInst* LI = dyn_cast<LoadInst>(I)) {      
+	      InjectingInstrumentationFunction<LoadInst>(I, F, M, LocationCounter);
+              ModifiedCodeGen = true;                                                                     
           }
-          else if(StoreInst* SI = dyn_cast<StoreInst>(I)){
-                  InjectingInstrumentationFunction<StoreInst>(I, F, M, LocationCounter);
-                  ModifiedCodeGen = true;
-          }
+	  else if(StoreInst* SI = dyn_cast<StoreInst>(I)){
+		  InjectingInstrumentationFunction<StoreInst>(I, F, M, LocationCounter);
+		  ModifiedCodeGen = true;
+	  }
         }
       }
-  }
+  } 
 }
     return ModifiedCodeGen;
 }
@@ -89,4 +99,3 @@ PassPluginLibraryInfo getPassPluginInfo() {
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
   return getPassPluginInfo();
 }
-
