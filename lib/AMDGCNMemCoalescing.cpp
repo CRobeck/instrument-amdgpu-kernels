3e4299b9180679d8607808eb4687b7eeafacd707
Corbin Robeck <corbin.robeck@amd.com>
2024-09-10 19:03:47 -0500

    clean up

--- lib/AMDGCNMemCoalescing.cpp
+++ lib/AMDGCNMemCoalescing.cpp
@@ -19,23 +19,10 @@
 using namespace llvm;
 using namespace std;
 
-
-std::string InstrumentationFunctionFile = instrumentation::utils::getenv("AMDCGN_INSTRUMENTATION_FILE");
-std::string InstrumentationFunctionName = instrumentation::utils::getenv("AMDCGN_INSTRUMENTATION_FUNCTION");
 bool AMDGCNMemCoalescing::runOnModule(Module &M) {
   bool ModifiedCodeGen = false;
   auto &CTX = M.getContext();
   uint32_t TtraceCounterInt = 1;
-//  const std::string &module_triple = M.getTargetTriple();
-//   if(module_triple != "amdgcn-amd-amdhsa") return ModifiedCodeGen;
-    std::string errorMsg;
-    std::unique_ptr<llvm::Module> InstrumentationModule;
-    if (!loadInstrumentationFile(InstrumentationFunctionFile, CTX, InstrumentationModule, errorMsg)) {
-      printf("error loading program '%s': %s", InstrumentationFunctionFile.c_str(),
-                 errorMsg.c_str());
-      exit(1);
-    }
-    Linker::linkModules(M, std::move(InstrumentationModule));  
   for (auto &F : M) {
     if(F.isIntrinsic()) continue;
     if (F.getCallingConv() == CallingConv::AMDGPU_KERNEL) {
@@ -54,25 +41,6 @@ bool AMDGCNMemCoalescing::runOnModule(Module &M) {
               
                 StringRef UnmangledName = getUnmangledName(F.getName());
              
-              SmallVector<StringRef, 10> BuiltinArgsTypeStrs;
-              std::string DemangledCall = demangle(std::string(InstrumentationFunctionName));
-               
-             StringRef DemangledCallStringRef = StringRef(DemangledCall);
-              StringRef BuiltinArgs =
-                  DemangledCallStringRef.slice(DemangledCallStringRef.find('(') + 1, DemangledCallStringRef.find(')'));   
-              BuiltinArgs.split(BuiltinArgsTypeStrs, ',', -1, false);
-              SmallVector<Type*, 10> ArgTypes;
-              for(int ArgIdx=0;ArgIdx<BuiltinArgsTypeStrs.size();ArgIdx++){
-                StringRef TypeStr = BuiltinArgsTypeStrs[ArgIdx].trim();   
-                if(TypeStr.ends_with("*")){
-                  ArgTypes.push_back(PointerType::get(CTX, 1));
-                }
-                else {
-                    Type* BaseType = parseBasicTypeName(TypeStr, CTX);
-                    ArgTypes.push_back(BaseType);
-                }
-              }
-
               DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc();
 //
               std::string SourceInfo =
@@ -84,11 +52,9 @@ bool AMDGCNMemCoalescing::runOnModule(Module &M) {
               DataLayout* dl = new DataLayout(&M);
               uint32_t typeSize = dl->getTypeStoreSize(dataType); 
               Value *typeSizeVal = Builder.getInt32(typeSize);
-              Function *InstrumentationFunction = M.getFunction("_Z13numCacheLinesPvjj");
-              Builder.CreateCall(FunctionType::get(Type::getVoidTy(CTX), {Addr->getType(), Type::getInt32Ty(CTX), Type::getInt32Ty(CTX)} ,false), InstrumentationFunction, {Addr, TtraceCounterIntVal, typeSizeVal});
-//
-//              Function *PrintFunction = M.getFunction("_Z15PrintCacheLinesj");
-//              Builder.CreateCall(FunctionType::get(Type::getVoidTy(CTX), {Type::getInt32Ty(CTX)}, false), PrintFunction, {Builder.getInt32(1)});
+              FunctionType *FT = FunctionType::get(Type::getVoidTy(CTX), {Addr->getType(), Type::getInt32Ty(CTX), Type::getInt32Ty(CTX)}, false);
+              FunctionCallee InstrumentationFunction = M.getOrInsertFunction("_Z13numCacheLinesPvjj", FT);
+			  Builder.CreateCall(InstrumentationFunction, {Addr, TtraceCounterIntVal, typeSizeVal});
               errs() << "Injecting Mem Coalescing Function Into AMDGPU Kernel: " << UnmangledName
                      << "\n";                     
               errs() << TtraceCounterInt << "     " << SourceInfo << "\n";
