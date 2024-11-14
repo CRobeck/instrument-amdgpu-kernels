f5f80909c70e6867ddd16558d272551d081204fa
Corbin Robeck <corbin.robeck@amd.com>
2024-11-14 13:39:01 +0000

    basic block timing

--- lib/AMDGCNMemTrace.cpp
+++ lib/AMDGCNMemTrace.cpp
@@ -11,66 +11,18 @@
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/IntrinsicsAMDGPU.h"
+#include "llvm/IR/Intrinsics.h"
 #include <iostream>
 #include <map>
 #include <system_error>
 #include <vector>
 
 using namespace llvm;
-using namespace std;
 
 std::string InstrumentationFunctionFile =
     instrumentation::utils::getenv("AMDCGN_INSTRUMENTATION_FUNCTIONS_FILE");
-
-std::map<int, std::string> AddrSpaceMap = {
-    {0, "FLAT"}, {1, "GLOBAL"}, {3, "SHARED"}, {4, "CONSTANT"}};
-
-std::string LoadOrStoreMap(const BasicBlock::iterator &I) {
-  if (LoadInst *LI = dyn_cast<LoadInst>(I))
-    return "LOAD";
-  else if (StoreInst *SI = dyn_cast<StoreInst>(I))
-    return "STORE";
-  else
-    throw std::runtime_error("Error: unknown operation type");
-}
-
-template <typename LoadOrStoreInst>
-void InjectInstrumentationFunction(const BasicBlock::iterator &I,
-                                   const Function &F, const llvm::Module &M,
-                                   uint32_t &LocationCounter, llvm::Value *Ptr,
-                                   bool PrintLocationInfo) {
-  auto &CTX = M.getContext();
-  auto LSI = dyn_cast<LoadOrStoreInst>(I);
-  if (not LSI)
-    return;
-  IRBuilder<> Builder(dyn_cast<Instruction>(I));
-  Value *Addr = LSI->getPointerOperand();
-  Value *LocationCounterVal = Builder.getInt32(LocationCounter);
-  Value *Op = LSI->getPointerOperand()->stripPointerCasts();
-  uint32_t AddrSpace = cast<PointerType>(Op->getType())->getAddressSpace();
-  // Skip shared and constant memory address spaces for now
-  if (AddrSpace == 3 || AddrSpace == 4)
-    return;
-  DILocation *DL = dyn_cast<Instruction>(I)->getDebugLoc();
-
-  std::string SourceInfo = (F.getName() + "     " + DL->getFilename() + ":" +
-                            Twine(DL->getLine()) + ":" + Twine(DL->getColumn()))
-                               .str();
-  //Function *InstrumentationFunction = M.getFunction("_Z8memTracePvjS_");
-  Function *InstrumentationFunction = M.getFunction("v_submit_address");
-  Builder.CreateCall(FunctionType::get(Type::getVoidTy(CTX),
-                                       {Ptr->getType(), Addr->getType(), 
-				       Type::getInt32Ty(CTX)},
-                                       false),
-                     InstrumentationFunction, {Ptr, Addr, LocationCounterVal});
-  if (PrintLocationInfo) {
-    errs() << "Injecting Mem Trace Function Into AMDGPU Kernel: " << SourceInfo
-           << "\n";
-    errs() << LocationCounter << "     " << SourceInfo << "     "
-           << AddrSpaceMap[AddrSpace] << "     " << LoadOrStoreMap(I) << "\n";
-  }
-  LocationCounter++;
-}
 
 
 bool AMDGCNMemTrace::runOnModule(Module &M) {
@@ -126,15 +78,15 @@ bool AMDGCNMemTrace::runOnModule(Module &M) {
     uint32_t LocationCounter = 0;
     for (Function::iterator BB = NF->begin(); BB != NF->end(); BB++) {
       for (BasicBlock::iterator I = BB->begin(); I != BB->end(); I++) {
-        if (LoadInst *LI = dyn_cast<LoadInst>(I)) {
-          InjectInstrumentationFunction<LoadInst>(I, *NF, M, LocationCounter,
-                                                  bufferPtr, true);
-          ModifiedCodeGen = true;
-        } else if (StoreInst *SI = dyn_cast<StoreInst>(I)) {
-          InjectInstrumentationFunction<StoreInst>(I, *NF, M, LocationCounter,
-                                                   bufferPtr, true);
-          ModifiedCodeGen = true;
-        }
+	  IRBuilder<> Builder(dyn_cast<Instruction>(I));
+  	  Function * ClockFunc = Intrinsic::getDeclaration(&M, Intrinsic::amdgcn_s_memrealtime, {});
+	  Value *CurrentCycles = Builder.CreateCall(ClockFunc, {});
+  	  //Function *InstrumentationFunction = M.getFunction("v_submit_address");
+	  //  Builder.CreateCall(FunctionType::get(Type::getVoidTy(CTX),
+	  //                                       {Ptr->getType(), Addr->getType(), 
+	  //        			       Type::getInt32Ty(CTX)},
+	  //                                       false),
+	  //                     InstrumentationFunction, {Ptr, Addr, LocationCounterVal});
       }
     }
   }
@@ -143,7 +95,7 @@ bool AMDGCNMemTrace::runOnModule(Module &M) {
 
 PassPluginLibraryInfo getPassPluginInfo() {
   const auto callback = [](PassBuilder &PB) {
-    PB.registerOptimizerLastEPCallback([&](ModulePassManager &MPM, auto) {
+    PB.registerOptimizerLastEPCallback([&](ModulePassManager &MPM, OptimizationLevel Level) {
       MPM.addPass(AMDGCNMemTrace());
       return true;
     });
